9957d88124026949e17b220824356cfac40a391d
Spete89 <spetet89@gmail.com>
2015-07-28 15:08:40 +0300

    New files

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+
+app.use(function(req,res,next){
+    
+    console.log(req.method);
+    next();
+});
+
+app.use(bodyParser());
+
+app.use('/', express.static('public'));
+
+app.post('/contact', function(req,res){
+    
+    console.log(req.body);
+    res.send("Your data is saved");
+    
+});
+
+
+ 
+app.listen(3000)
